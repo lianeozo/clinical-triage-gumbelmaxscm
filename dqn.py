5c3262cb0a2f89b58da0003adb8fa5113820ac3a
KC10201 <70864069+KC10201@users.noreply.github.com>
2026-03-04 22:18:42 -0800

    Update dqn.py
    Add replay buffer and DQN network classes

--- dqn.py
+++ dqn.py
@@ -1,1 +1,55 @@
+import numpy as np
+import random
+from collections import namedtuple, deque
+import math
+from matplotlib import pyplot as plt
+import matplotlib
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQN(nn.Module):
+    def __init__(self, n_observations, hidden_dim, n_actions):
+        super(DQN, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(n_observations, hidden_dim),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_dim, n_actions),
+            nn.Sigmoid()
+        )
+
+    def forward(self, obs):
+        output = self.network(obs)
+        return output
+
 
